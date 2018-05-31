9c8e6088f7dd5dd24491bd6d633cdcb5765fc854
Jon Gear <askjon@jongear.io>
2018-05-31 12:43:26 -0500

    fix deploy

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -3,6 +3,17 @@
 echo '###### Starting API deployment! ######'
 
 cd api
+
+cat <<EOF > env.yml
+dev:
+    DYNAMODB_TABLE: "dev-heroes"
+
+EOF
+
+git add -f env.yml
+git add -f node_modules
+git commit -am "local commit to expose to serverless" --quiet
+
 sls deploy --stage dev --verbose --force
 
 echo '###### API deployment finished! ######'
