f404cc22af7d34909722b5e1ac64c89a9c41440c
Jon Gear <askjon@jongear.io>
2018-06-03 14:39:35 -0500

    add url notes

--- website/src/environments/environment.dev.ts
+++ website/src/environments/environment.dev.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000"
+  apiUrl: "https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev" // replaces me with the API Gateway uri or a Custom Domain Name
 };
 
 /*
--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/prod" // replaces me with the API Gateway uri or a Custom Domain Name
 };
